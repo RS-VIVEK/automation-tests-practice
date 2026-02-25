766441d9b0445967af9d18b27727675fcb6a3f38
RS-VIVEK <vivekvivekrs07@gmail.com>
2026-02-25 12:14:25 +0530

    Updated factorial class

--- src/test/java/Programs/FactorialNo.java
+++ src/test/java/Programs/FactorialNo.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class FactorialNo {
 
+    //factorial no program
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number");
